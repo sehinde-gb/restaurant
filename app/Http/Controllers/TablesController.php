91125762313f204ca3b121538700eff2671d8a0f
yameste <yameste@yahoo.co.uk>
2020-02-18 15:18:09 +0000

    added logic to book the tables that are free within an array of if they cannot be booked they are in an empty array

--- app/Http/Controllers/TablesController.php
+++ app/Http/Controllers/TablesController.php
@@ -33,88 +33,156 @@ class TablesController extends Controller
      */
     public function index()
     {
-       
-            $tables = Table::all()->toArray();
+            // Push all available tables in to an array.
+            //$tables = Table::all()->toArray();
             //dd($tables);    
-             
+            
+            /*
+            Set up tables array with a list of tables and the properties.
+            */
+
+            $tables = [
+                ["id" => 1, "size" => 6, "status" => "booked"],
+                ["id" => 2, "size" => 4, "status" => "free"],
+                ["id" => 3, "size" => 2, "status" => "free"],
+                ["id" => 4, "size" => 6, "status" => "free"],
+                ["id" => 5, "size" => 2, "status" => "free"],
+                ["id" => 6, "size" => 8, "status" => "free"],
+                ["id" => 7, "size" => 2, "status" => "booked"],
+                ["id" => 8, "size" => 4, "status" => "free"],
+                ["id" => 9, "size" => 4, "status" => "free"],
+                ["id" => 10, "size" => 12, "status" => "booked"],
+                ["id" => 11, "size" => 20, "status" => "free"],
+                ["id" => 12, "size" => 12, "status" => "free"],
+            ];
+            
+
+            // Search through the list of tables and filter through and pluck out the ones that are free.
 
             $filter = "free";
 
-            $new_array = array_filter($tables, function($var) use ($filter)  {
+            // Iterate over the table collection and pluck out the tables that have their status set to free
+            $array_free = array_filter($tables, function($var) use ($filter)  {
                 return ($var['status'] == $filter);
             });
 
-            dd($new_array);
+            /* Using a switch statement to determine the number of persons booked and we return the lowest 
+            minimum number of tables available or an empty array if it cannot be booked. */
 
+            $persons = 2;
             
-            $min = PHP_INT_MAX;
-            foreach($new_array as $value) { 
-            if($value['size'] < $min) {
-                $tables_ids = $value['id'];
-                $min = $value['size'];
-            }
 
-            dd($tables_ids);
-
-            // result table_ids
-            // search for size
-            
-           
-            $persons = 10;
-  
             switch ($persons) {
-                case $persons == 10;
-                
-                $big = array_search('8', array_column($new_array, 'size'));
-                $small = array_search('2', array_column($new_array, 'size'));
-                echo $big;
-                echo $small;
 
-                $tables_ids = [];
-                array_push($tables_ids, $big, $small);
-                    
-            break;
+
+                case $persons == 10;
+
+                $arr = array_filter($array_free, function($ar) {
+                    return ($ar['size'] == 8 || $ar['size'] == 2 && $ar['id'] < 5);
+                });
                 
+                //dd($arr);
+                $tables_ids = array_column($arr, 'id');
+    
+                break;
+
                 case $persons == 9;
-                    # code...
-                    break;
+
+                $arr = array_filter($array_free, function($ar) {
+                    return (($ar['size'] == 9)  && $ar['id'] < 5);
+                });
+                
+                //dd($arr);
+                $tables_ids = array_column($arr, 'id');
+    
+                break;
 
                 case $persons == 8;
-                    # code...
-                    break;
-                
+
+                $arr = array_filter($array_free, function($ar) {
+                    return ($ar['size'] == 6 || $ar['size'] == 2 && $ar['id'] < 5);
+                });
+
+                //dd($arr);
+                $tables_ids = array_column($arr, 'id');
+
+                break;
+
                 case $persons == 7;
-                    # code...
-                    break;
-                
+
+                $arr = array_filter($array_free, function($ar) {
+                    return (($ar['size'] == 7) && $ar['id'] < 5);
+                });
+
+                $tables_ids = array_column($arr, 'id');
+
+                break;
+
                 case $persons == 6;
-                    # code...
-                    break;
-                
+
+                $arr = array_filter($array_free, function($ar) {
+                    return (($ar['size'] == 4 || $ar['size'] == 2) && $ar['id'] < 5);
+                });
+
+                //dd($arr);
+                $tables_ids = array_column($arr, 'id');
+
+                break;
+
                 case $persons == 5;
-                    # code...
-                    break;   
+
+                $arr = array_filter($array_free, function($ar) {
+                    return (($ar['size'] == 5) && $ar['id'] < 5);
+                });
+
+                $tables_ids = array_column($arr, 'id');
+                break;
 
                 case $persons == 4;
-                    # code...
-                    break;    
-                
+
+                $arr = array_filter($array_free, function($ar) {
+                    return (($ar['size'] == 4) && $ar['id'] < 5);
+                });
+
+                $tables_ids = array_column($arr, 'id');
+
+                break;
+
                 case $persons == 3;
-                    # code...
-                    break;    
-                
+
+                $arr = array_filter($array_free, function($ar) {
+                    return (($ar['size'] == 3) && $ar['id'] < 5);
+                });
+
+                $tables_ids = array_column($arr, 'id');
+
+                break;
+
                 case $persons == 2;
-                    # code...
-                    break;    
-               
-                case $persons == 1;
-                    # code...
-                    break;    
+
+                $arr = array_filter($array_free, function($ar) {
+                    return (($ar['size'] == 2) && $ar['id'] < 5);
+                });
+
+                $tables_ids = array_column($arr, 'id');
+                dd($tables_ids);
+
+                break;
+
             }
+
+
+            
+
+            
+            
+           
+            
+            
 
         //return view('tables.index', ['tables' => $tables]);
    
-    }
+    
     }
 }
     
